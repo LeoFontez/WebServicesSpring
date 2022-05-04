8659bfb739f7ee708a1d4bd8f942073dbbc7d2c7
Leo Fontes <79946719+LeoFontez@users.noreply.github.com>
2022-05-04 19:36:04 -0300

    Update WebServicesSpringApplication.java

--- src/main/java/com/project/course/WebServicesSpringApplication.java
+++ src/main/java/com/project/course/WebServicesSpringApplication.java
@@ -8,6 +8,7 @@ public class WebServicesSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebServicesSpringApplication.class, args);
+		System.out.println("a");
 	}
 
 }

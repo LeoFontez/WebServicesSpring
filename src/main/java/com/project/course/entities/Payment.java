0758cabebbb267967c605b5a88bc88206d24f19a
Leo Fontes <79946719+LeoFontez@users.noreply.github.com>
2022-05-07 14:33:09 -0300

    Subtotal & Total methods

--- src/main/java/com/project/course/entities/Payment.java
+++ src/main/java/com/project/course/entities/Payment.java
@@ -2,7 +2,6 @@ package com.project.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
-import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,22 +11,24 @@ import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 @Table(name = "tb_payment")
-public class Payment implements Serializable{
+public class Payment implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private Instant moment;
 	
+	@JsonIgnore
 	@OneToOne
 	@MapsId
 	private Order order;
 	
 	public Payment() {
-		
 	}
 
 	public Payment(Long id, Instant moment, Order order) {
@@ -53,17 +54,20 @@ public class Payment implements Serializable{
 		this.moment = moment;
 	}
 
-	public Order getOder() {
+	public Order getOrder() {
 		return order;
 	}
 
-	public void setOder(Order order) {
+	public void setOrder(Order order) {
 		this.order = order;
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -75,8 +79,11 @@ public class Payment implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		Payment other = (Payment) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-	
-	
 }
